ac7ecc3185efa84364fdb000424ddbb64abc383c
Crowdbotics <team@crowdbotics.com>
2023-08-24 14:02:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cold-limit-43053",
+  url: "https://cold-limit-43053.botics.co",
+  api: "https://cold-limit-43053.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
